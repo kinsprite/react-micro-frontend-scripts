ab998aa7cea75501dae13a55fa1713ee57c83400
kinsprite <qxqs11@126.com>
2020-06-27 22:53:57 +0800

    always save git revision short

--- scripts/internal/getGitTagOrShort.js
+++ scripts/internal/getGitTagOrShort.js
@@ -3,14 +3,16 @@ const git = require('git-rev-sync');
 const getAppDir = require('./getAppDir');
 
 function getGitTagOrShort() {
+  const long = git.long(getAppDir());
+  // Generally, eight to ten characters are more than enough to be unique within a project.
+  const short = long.substr(0, 8);
+
   if (git.isTagDirty()) {
-    const long = git.long(getAppDir());
-    // Generally, eight to ten characters are more than enough to be unique within a project.
-    return { short: long.substr(0, 8) };
+    return { tag: '', short };
   }
 
   const tag = git.tag();
-  return { tag };
+  return { tag, short };
 }
 
 module.exports = getGitTagOrShort;
